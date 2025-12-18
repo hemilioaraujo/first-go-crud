9acb98acafcf69589fb09cc336e8f3aece06c578
hemilioaraujo <hemilioaraujo@gmail.com>
2025-12-18 14:51:57 -0300

    feat: Initialize Go project with environment variable loading and structured REST API error handling.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hemilioaraujo/first-crud-go
+
+go 1.25.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	testEnv := os.Getenv("TESTE")
+	fmt.Println(testEnv)
+}
